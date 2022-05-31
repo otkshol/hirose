58a3ad919e39ffe41763ed90763389bd2fd220fe
otkshol <charm.ot.trog@gmail.com>
2022-05-31 09:08:06 +0900

    Initial commit

---
+++ src/main/kotlin/com/project/hirose/HiroseApplication.kt
@@ -0,0 +1,11 @@
+package com.project.hirose
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HiroseApplication
+
+fun main(args: Array<String>) {
+	runApplication<HiroseApplication>(*args)
+}
